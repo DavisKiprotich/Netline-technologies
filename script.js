baebfbd6a3ee464d67da8f0a7bf9890ecad5ed22
Kiprotich Davis <mylesdavyz@gmail.com>
2023-10-02 12:17:20 +0300

    MEDIA QUERY FOR SMALL DEVICES

--- script.js
+++ script.js
@@ -8,17 +8,34 @@ playIcon.addEventListener("click", function () {
   }, 1000);
 });
 
-const video = document.getElementById('background-video');
-const videos = ['video1.mp4', 'video2.mp4', 'video3.mp4']; // Add more video filenames if needed
+const video = document.getElementById("background-video");
+const videos = ["video1.mp4", "video2.mp4", "video3.mp4"]; // Add more video filenames if needed
 let currentVideoIndex = 0;
 
-video.addEventListener('ended', playNextVideo);
+video.addEventListener("ended", playNextVideo);
 
 function playNextVideo() {
-    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
-    video.src = videos[currentVideoIndex];
-    video.play();
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+  video.src = videos[currentVideoIndex];
+  video.play();
 }
 
 // Initial video play
 video.play();
+
+// Hamburger and close display
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navLinks = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.add("show");
+  hamburger.style.display = "none";
+  close.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  navLinks.classList.remove("show");
+  close.style.display = "none";
+  hamburger.style.display = "block";
+});
